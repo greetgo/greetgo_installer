9d594945dc6691b37ba3e659b4f4ad3e52b84246
ekolpakov <ekolpakov@greet-go.com>
2017-02-07 14:20:11 +0600

    Added two projects: greetgo.class_scanner and greetgo.util

--- src/kz/greetgo/installer/Commander.java
+++ src/kz/greetgo/installer/Commander.java
@@ -1,8 +1,74 @@
 package kz.greetgo.installer;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Commander {
 
-  public static Commander aboutDir(String currentDir) {
-    return null;
+  private final File current;
+
+  private Commander(File current) {
+    this.current = current;
+  }
+
+  public static Commander aboutDir(@SuppressWarnings("SameParameterValue") String currentDir) {
+    if (currentDir == null) currentDir = ".";
+    File current = new File(currentDir);
+    return new Commander(current);
+  }
+
+  public static Commander current() {
+    return aboutDir(null);
+  }
+
+  public void check() {
+    if (!current.isDirectory()) throw new IllegalArgumentException(current.toPath() + " is not a directory");
+  }
+
+  public Commander cd(String subPath) {
+    return new Commander(current.toPath().resolve(subPath).toFile());
+  }
+
+  public void cmd(String commandWithArguments) throws IOException, InterruptedException {
+    run(commandWithArguments.split("\\s+"));
+  }
+
+  public int cmdExitCode(String commandWithArguments) throws IOException, InterruptedException {
+    return runExitCode(commandWithArguments.split("\\s+"));
+  }
+
+  public Commander mkdirs() {
+    current.mkdirs();
+    return this;
+  }
+
+  public void run(String... commandArray) throws IOException, InterruptedException {
+    int exitCode = runExitCode(commandArray);
+    if (exitCode == 0) return;
+    throw new RuntimeException("Exit code = " + exitCode + " for: "
+      + Arrays.stream(commandArray).collect(Collectors.joining(", ")));
+  }
+
+  public int runExitCode(String... commandArray) throws IOException, InterruptedException {
+    check();
+    Process process = new ProcessBuilder(commandArray)
+      .directory(current)
+      .inheritIO()
+      .start();
+    return process.waitFor();
+  }
+
+  public String pwd() {
+    try {
+      return current.getAbsoluteFile().getCanonicalPath();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public boolean exists() {
+    return current.exists();
   }
 }
--- src/kz/greetgo/installer/InstallerTask.java
+++ src/kz/greetgo/installer/InstallerTask.java
@@ -1,9 +1,69 @@
 package kz.greetgo.installer;
 
-public class InstallerTask {
-  public static void main(String[] args) {
-    Commander commander = Commander.aboutDir(".");
+import java.util.ArrayList;
+import java.util.List;
 
-    System.out.println("commander = " + commander);
+public class InstallerTask {
+  public static void main(String[] args) throws Exception {
+    InstallerTask it = new InstallerTask();
+    it.initRepos();
+    it.execute();
+  }
+
+  Commander current = Commander.current();
+
+  class Repo {
+    public final String repo, subDir;
+
+    public final List<String> versionCommitList = new ArrayList<>();
+
+    Repo(String repo, String subDir) {
+      this.repo = repo;
+      this.subDir = subDir;
+    }
+
+    public Repo commit(String id) {
+      versionCommitList.add(id);
+      return this;
+    }
+  }
+
+  private void initRepos() {
+    addRepo("https://github.com/greetgo/greetgo.util.git", "greetgo.util/greetgo.util")
+      .commit("12b46d0 - version 0.0.26")
+      .commit("2b1413d - version 0.0.27")
+    ;
+    addRepo("https://github.com/greetgo/greetgo.class_scanner.git", "greetgo.class_scanner")
+      .commit("aaf0226 - version 0.0.1")
+    ;
+  }
+
+  final List<Repo> repoList = new ArrayList<>();
+
+  private Repo addRepo(String repo, String subDir) {
+    Repo ret = new Repo(repo, subDir);
+    repoList.add(ret);
+    return ret;
+  }
+
+  private void execute() throws Exception {
+    Commander ip = current.cd("build/InstallingProjects");
+    ip.mkdirs().check();
+
+    for (Repo repo : repoList) {
+      Commander prg = ip.cd(repo.subDir);
+      if (!prg.exists()) ip.cmd("git clone " + repo.repo);
+      prg.check();
+      prg.cmdExitCode("git pull");
+      for (String commitVersion : repo.versionCommitList) {
+        String[] split = commitVersion.split("-");
+        String commit = split[0].trim();
+        String version = split[1].trim();
+        System.out.println("git checkout " + commit + " for " + version);
+        prg.cmd("git checkout " + commit);
+        prg.cmd("gradle clean install");
+      }
+
+    }
   }
 }
